f4ccee134961ffc943b03bad0cfc64bf9ca5568d
Olorundare Abiola <realaisha1@gmail.com>
2024-12-08 01:30:08 +0100

    Reapply .gitignore rules and remove cached files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html','./loop1/step1/index.html', './styles.css'],
+  theme: {
+    extend: {
+      backgroundColor: ["hover"],
+    },
+  },
+  plugins: [],
+};
+
+
