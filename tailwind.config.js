f0880bfbb0180d7e8721d8b1d706a4bccfeda204
Olorundare Abiola <realaisha1@gmail.com>
2025-02-12 15:34:14 +0100

    Adding step 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html','./loop1/step1/index.html', './styles.css'],
+  content: ['./index.html',
+    './loop1/step1/index.html',
+    './loop1/step2/index.html', './styles.css'],
   theme: {
     extend: {
       backgroundColor: ["hover"],
