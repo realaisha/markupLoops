14fcaa6e24d83e45d05fe040149b57e52302f1f7
Olorundare Abiola <realaisha1@gmail.com>
2025-03-20 15:32:10 +0100

    updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./index.html',
     './loop1/step1/index.html',
-    './loop1/step2/index.html', './styles.css'],
+    './loop1/step2/index.html',
+    './loop1/step3/index.html',
+    './loop1/step4/index.html',
+    './loop1/step5/index.html',
+    './loop1/step6/index.html',
+    './loop1/step7/index.html',
+    './loop1/step8/index.html',
+    './loop1/step9/index.html', './styles.css'],
   theme: {
     extend: {
       backgroundColor: ["hover"],
